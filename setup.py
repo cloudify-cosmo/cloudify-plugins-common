770adf7e75738e56a404abcb5e49a94f9df6aa3f
Dan Kilman <dankilman@gmail.com>
2013-11-14 17:56:20 +0200

    Bump to version: 0.2 and use plugins with this tag.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cosmo-celery-common',
-    version='0.1.1',
+    version='0.2',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=['cosmo'],
