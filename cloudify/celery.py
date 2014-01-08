81b8fe8406ff03ec7caa83400de94ace7cf3a5aa
Idan Moyal <idan@gigaspaces.com>
2014-01-08 12:22:39 +0200

    CLOUDIFY-2336 added cloudify module.

---
+++ cloudify/celery.py
@@ -0,0 +1,56 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+"""
+This module is not intended to be used as standalone.
+On celery worker installation this file will be copied to the application root directory.
+"""
+
+from __future__ import absolute_import
+
+__author__ = 'idanmo'
+
+import sys
+import traceback
+import os
+
+from celery import Celery
+from celery.signals import after_setup_task_logger
+
+from cloudify.utils import build_includes
+from cloudify.logging import setup_logger
+
+
+celery = Celery('cosmo.celery', include=build_includes(os.path.dirname(__file__)))
+
+current_excepthook = sys.excepthook
+
+
+def new_excepthook(exception_type, value, the_traceback):
+    with open(os.path.expanduser('~/celery_error.out'), 'w') as f:
+        f.write('Type: {0}\n'.format(exception_type))
+        f.write('Value: {0}\n'.format(value))
+        traceback.print_tb(the_traceback, file=f)
+    current_excepthook(type, value, the_traceback)
+
+sys.excepthook = new_excepthook
+
+
+@after_setup_task_logger.connect
+def setup_cloudify_logger(loglevel=None, **kwargs):
+    setup_logger(loglevel, kwargs)
+
+if __name__ == '__main__':
+    celery.start()
---
+++ cloudify/utils.py
@@ -0,0 +1,71 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+__author__ = 'idanmo'
+
+import fnmatch
+import os
+from os import path
+from constants import CLOUDIFY_APP_DIR_KEY
+
+
+def get_app_dir():
+    """
+    Returns the directory celery tasks are stored within.
+    """
+    return os.environ[CLOUDIFY_APP_DIR_KEY]
+
+
+def get_cosmo_properties():
+    management_ip_key = "MANAGEMENT_IP"
+    agent_ip_key = "AGENT_IP"
+    if management_ip_key not in os.environ:
+        raise RuntimeError("{0} is not set in environment".format(management_ip_key))
+    if agent_ip_key not in os.environ:
+        raise RuntimeError("{0} is not set in environemnt".format(agent_ip_key))
+    return {
+        "management_ip": os.environ[management_ip_key],
+        "ip": os.environ[agent_ip_key]
+    }
+
+
+def build_includes(celery_app_root_dir):
+    """
+    Returns a list of celery included modules (all python modules under root dir).
+    """
+    if not path.exists(celery_app_root_dir):
+        raise IOError("Celery application root directory: {0} not found".format(celery_app_root_dir))
+
+    includes = []
+
+    app_root_dir = path.realpath(path.join(celery_app_root_dir, '..'))
+
+    for root, dirnames, filenames in os.walk(celery_app_root_dir):
+        for filename in fnmatch.filter(filenames, '*.py'):
+            includes.append(os.path.join(root, filename))
+
+    # remove .py suffix from include
+    includes = map(lambda include: include[:-3], includes)
+
+    # remove path prefix to start with cosmo
+    includes = map(lambda include: include.replace(app_root_dir, ''), includes)
+
+    # replace slashes with dots in include path
+    includes = map(lambda include: include.replace('/', '.'), includes)
+
+    # remove the dot at the start
+    includes = map(lambda include: include[1:], includes)
+
+    return includes
