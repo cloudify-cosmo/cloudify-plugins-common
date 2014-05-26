434644b459c3152b250b6b6a85239c5f678910f6
Dan Kilman <dankilman@gmail.com>
2014-05-26 10:53:08 +0300

    CFY-642 add license header

--- cloudify/workflows/tasks.py
+++ cloudify/workflows/tasks.py
@@ -13,9 +13,10 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+__author__ = 'dank'
+
 
 import uuid
-
 
 from cloudify.celery import celery as celery_client
 
