051ea395bd3924c3e43bfc9a504ad59a52ff54cf
nir0s <nir36g@gmail.com>
2014-08-26 15:01:26 +0300

    still testing

--- cloudify/context.py
+++ cloudify/context.py
@@ -96,9 +96,12 @@ class ContextCapabilities(object):
     @property
     def _capabilities(self):
         if self._relationship_runtimes is None:
-            self._relationship_runtimes = {
-                rel_id: get_node_instance(rel_id).runtime_properties
-                for rel_id in self._relationships}
+            self._relationship_runtimes = dict(
+                (rel_id, get_node_instance(rel_id).runtime_properties)
+                for rel_id in self._relationships)
+            # self._relationship_runtimes = {
+            #     rel_id: get_node_instance(rel_id).runtime_properties
+            #     for rel_id in self._relationships}
         return self._relationship_runtimes
 
 
