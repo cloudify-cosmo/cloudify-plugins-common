940680187fb149df8b3e5b6cf3ce2b7857b3df2a
Isaac Shabtay <isaac@shabtay.com>
2015-09-18 14:48:04 -0700

    CFY-3612 Added type and type_hierarchy to nodes

---
+++ cloudify/tests/workflows.py
@@ -0,0 +1,24 @@
+########
+# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from cloudify.decorators import workflow
+
+
+@workflow
+def execute_operation(ctx, operation, testing, nodes, **_):
+    for node_id in nodes:
+        node = ctx.get_node(node_id)
+        instance = next(node.instances)
+        instance.execute_operation(operation, kwargs={'testing': testing})
