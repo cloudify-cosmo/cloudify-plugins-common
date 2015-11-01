31fbfc02d41e99bff752cd9c1fdc57f94e4edcb5
opencm <limor@gigaspaces.com>
2015-11-01 14:19:02 +0200

    Bump version to 3.3rc1

--- packaging/provision.sh
+++ packaging/provision.sh
@@ -69,10 +69,10 @@ function print_params(){
 
 # VERSION/PRERELEASE/BUILD must be exported as they is being read as an env var by the cloudify-agent-packager
 export VERSION="3.3.0"
-export PRERELEASE="m7"
-export BUILD="277"
-CORE_TAG_NAME="3.3m7"
-PLUGINS_TAG_NAME="1.3m7"
+export PRERELEASE="rc1"
+export BUILD="278"
+CORE_TAG_NAME="3.3rc1"
+PLUGINS_TAG_NAME="1.3rc1"
 
 #env Variables
 GITHUB_USERNAME=$1
