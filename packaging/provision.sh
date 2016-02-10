c05580385496e9c59a53eb4ac329ce047770ebd7
Limor Shemesh <limor@Limors-MacBook-Pro.local>
2016-02-10 18:46:21 +0200

    update-pip-version

--- packaging/provision.sh
+++ packaging/provision.sh
@@ -17,7 +17,7 @@ function install_dependencies(){
         return
     fi
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python &&
-    sudo pip install pip==7.1.2 --upgrade
+    sudo pip install '>=8.0.0,<9.0.0' --upgrade
 }
 
 function install_wagon(){
