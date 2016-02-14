1c211b812e103fb6655d75aefac4395ccb7ce2c0
nir0s <nir36g@gmail.com>
2016-02-14 10:19:32 +0200

    CFY-4881 finalizations

--- packaging/blueprint/provision.sh
+++ packaging/blueprint/provision.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+curl $(ctx node properties common_builder_script_url) -o ./common-provision.sh && source common-provision.sh
+
 SUDO=""
 
-function install_dependencies(){
-    echo "## Installing necessary dependencies"
+function install_dependencies() {
+    ctx logger info 'Installing necessary dependencies...'
 
     if  which yum; then
         sudo yum -y install python-devel gcc openssl git libxslt-devel libxml2-devel openldap-devel
@@ -13,43 +15,30 @@ function install_dependencies(){
         sudo apt-get -y install build-essential python-dev
         SUDO="sudo"
     else
-        echo 'probably windows machine'
+        ctx logger info 'Probably a Windows machine...'
         return
     fi
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python &&
     sudo pip install pip==7.1.2 --upgrade
 }
 
-function install_wagon(){
-    echo "## installing wagon"
+function install_wagon() {
+    ctx logger info 'Installing Wagon...'
     $SUDO pip install wagon==0.3.0
 }
 
-function wagon_create_package(){
-    echo "## wagon create package"
+function create_wagon() {
+    ctx logger info 'Creating Plugin Wagon...'
     $SUDO wagon create -s https://github.com/cloudify-cosmo/$PLUGIN_NAME/archive/$PLUGIN_TAG_NAME.tar.gz -r --validate -v -f
 }
 
-
-
-# VERSION/PRERELEASE/BUILD must be exported as they is being read as an env var by the cloudify-agent-packager
-CORE_TAG_NAME=$(ctx node properties core_tag_name)
-COMMON_BUILDER_SCRIPT=$(ctx node properties common_builder_script_url)
-
-GITHUB_USERNAME=$(ctx node properties github_username)
-GITHUB_PASSWORD=$(ctx node properties github_password)
-AWS_ACCESS_KEY_ID=$(ctx node properties aws_access_key_id)
-AWS_ACCESS_KEY=$(ctx node properties aws_access_key)
 PLUGIN_NAME=$(ctx node properties plugin_name)
 PLUGIN_TAG_NAME=$(ctx node properties plugin_tag_name)
-
-curl $COMMON_BUILDER_SCRIPT -o ./common-provision.sh && source common-provision.sh
-export AWS_S3_PATH="org/cloudify3/wagons/$PLUGIN_NAME/$PLUGIN_TAG_NAME"
 
 print_params
 install_dependencies &&
 install_wagon &&
-wagon_create_package &&
+create_wagon &&
 create_md5 "wgn" &&
 [ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "wgn" && upload_to_s3 "md5"
 
