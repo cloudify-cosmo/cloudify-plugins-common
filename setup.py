83cefc889f1620f8b4ee75c889f2f9b3d12cb8f6
opencm <limor@gigaspaces.com>
2016-10-19 14:41:56 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 from setuptools import setup
 
 install_requires = [
-    'cloudify-rest-client==4.0a5',
+    'cloudify-rest-client==4.0a6',
     'pika==0.9.14',
     'networkx==1.8.1',
     'proxy_tools==0.1.0',
@@ -43,7 +43,7 @@ except ImportError:
 
 setup(
     name='cloudify-plugins-common',
-    version='4.0a5',
+    version='4.0a6',
     author='cosmo-admin',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify',
