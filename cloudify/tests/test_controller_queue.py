ea1ce9aea5e594d77135ab92c15d773c6cd6c044
Omer Fogel <omer@omers-mbp.home>
2017-01-30 20:13:43 +0200

    add types.yaml to controller tests

--- cloudify/tests/test_controller_queue.py
+++ cloudify/tests/test_controller_queue.py
@@ -4,14 +4,14 @@ import testtools
 from cloudify.test_utils import workflow_test
 
 
-class NodeContextTests(testtools.TestCase):
+class ControllertTests(testtools.TestCase):
 
     test_blueprint_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "resources/blueprints/test-controller-queue.yaml")
 
     @workflow_test(test_blueprint_path)
-    def test_node_type(self, cfy_local):
+    def test_controller_queue_property(self, cfy_local):
         cfy_local.execute('install')
 
         print "aaa!!!"
