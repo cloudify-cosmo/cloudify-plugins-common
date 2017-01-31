b8cbf436d8843fa5de1670826e2e202cbb68517b
Omer Fogel <omer@omers-mbp.home>
2017-01-31 23:22:49 +0200

    add types.yaml to controller tests

--- cloudify/tests/test_controller_queue.py
+++ cloudify/tests/test_controller_queue.py
@@ -10,21 +10,21 @@ class ControllertTests(testtools.TestCase):
         os.path.dirname(os.path.realpath(__file__)),
         "resources/blueprints/test-controller-queue.yaml")
 
-    @workflow_test(test_blueprint_path)
+    @workflow_test(blueprint_path=test_blueprint_path)
     def test_controller_queue_property(self, cfy_local):
         cfy_local.execute('install')
 
-        instance = cfy_local.storage.get_node_instances(node_id='direct')
+        instance = cfy_local.storage.get_node_instances(node_id='direct')[0]
         self.assertEqual(
             instance.properties['controller_queue'], 'direct')
-        instance = cfy_local.storage.get_node_instances(node_id='host_none')
+        instance = cfy_local.storage.get_node_instances(node_id='host_none')[0]
         self.assertEqual(instance.properties['controller,queue'], '')
-        instance = cfy_local.storage.get_node_instances('connected_host')
+        instance = cfy_local.storage.get_node_instances('connected_host')[0]
         self.assertEqual(
             instance.properties['controller_queue'], 'queue')
-        instance = cfy_local.storage.get_node_instances(node_id='direct_override')
+        instance = cfy_local.storage.get_node_instances(node_id='direct_override')[0]
         self.assertEqual(
             instance.properties['controller_queue'], 'direct_override')
-        instance = cfy_local.storage.get_node_instances(node_id='contained_node')
+        instance = cfy_local.storage.get_node_instances(node_id='contained_node')[0]
         self.assertEqual(
             instance.properties['controller_queue'], 'father_host')
