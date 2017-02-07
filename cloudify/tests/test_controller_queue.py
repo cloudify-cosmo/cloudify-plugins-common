b72787c7c119e7eadd6a214750cdb5b7ff9688f4
Omer Fogel <omer@Omers-MacBook-Pro.local>
2017-02-07 13:01:50 +0200

    add types.yaml to controller tests

--- cloudify/tests/test_controller_queue.py
+++ cloudify/tests/test_controller_queue.py
@@ -10,26 +10,25 @@ class ControllertTests(testtools.TestCase):
         os.path.dirname(os.path.realpath(__file__)),
         "resources/blueprints/test-controller-queue.yaml")
 
-    @workflow_test(blueprint_path=test_blueprint_path,
-                   resources_to_copy=[
-                       'resources/blueprints/execute_operation_workflow.yaml'])
+    @workflow_test(blueprint_path=test_blueprint_path)
     def test_controller_queue_property(self, cfy_local):
         cfy_local.execute('install')
 
-        instance = cfy_local.storage.get_node_instances('direct')[0]
-        self.assertEqual(
-            instance.properties['controller_queue'], 'direct')
-        instance = cfy_local.storage.get_node_instances('host_none')[0]
-        self.assertEqual(instance.properties['controller,queue'], '')
-        instance = cfy_local.storage.get_node_instances(
-            'connected_host')[0]
-        self.assertEqual(
-            instance.properties['controller_queue'], 'queue')
-        instance = cfy_local.storage.get_node_instances(
-            'direct_override')[0]
-        self.assertEqual(
-            instance.properties['controller_queue'], 'direct_override')
-        instance = cfy_local.storage.get_node_instances(
-            'contained_node')[0]
-        self.assertEqual(
-            instance.properties['controller_queue'], 'father_host')
+        print cfy_local
+
+        direct = cfy_local.storage.get_node_instances('direct')[0]
+        print direct
+        host_none = cfy_local.storage.get_node_instances('host_none')[0]
+        print host_none
+        connected_host = cfy_local.storage.get_node_instances('connected_host')[0]
+        print connected_host
+        direct_override = cfy_local.storage.get_node_instances('direct_override')[0]
+        print direct_override
+        contained_node = cfy_local.storage.get_node_instances('contained_node')[0]
+        print contained_node
+
+        self.assertEqual(direct.runtime_properties['controller_queue'], 'direct')
+        self.assertEqual(host_none.runtime_properties['controller,queue'], '')
+        self.assertEqual(connected_host.runtime_properties['controller_queue'], 'queue')
+        self.assertEqual(direct_override.runtime_properties['controller_queue'], 'direct_override')
+        self.assertEqual(contained_node.runtime_properties['controller_queue'], 'father_host')
