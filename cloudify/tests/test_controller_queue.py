e2b43dcd1ad6cfff362fb382de63e6d9be31d1e7
earthmant <earthmant@icloud.com>
2017-02-07 21:38:24 +0200

    fixing flake8 in test_controller

--- cloudify/tests/test_controller_queue.py
+++ cloudify/tests/test_controller_queue.py
@@ -4,7 +4,6 @@ import testtools
 from cloudify.test_utils import workflow_test
 
 
-
 class ControllertTests(testtools.TestCase):
 
     test_blueprint_path = os.path.join(
@@ -15,19 +14,34 @@ class ControllertTests(testtools.TestCase):
     def test_controller_queue_property(self, cfy_local):
         cfy_local.execute('install')
 
-        direct = cfy_local.storage.get_node_instances('direct')[0]
+        direct = \
+            cfy_local.storage.get_node_instances('direct')[0]
         print direct
-        host_none = cfy_local.storage.get_node_instances('host_none')[0]
+        host_none = \
+            cfy_local.storage.get_node_instances('host_none')[0]
         print host_none
-        connected_host = cfy_local.storage.get_node_instances('connected_host')[0]
+        connected_host = \
+            cfy_local.storage.get_node_instances('connected_host')[0]
         print connected_host
-        direct_override = cfy_local.storage.get_node_instances('direct_override')[0]
+        direct_override = \
+            cfy_local.storage.get_node_instances('direct_override')[0]
         print direct_override
-        contained_node = cfy_local.storage.get_node_instances('contained_node')[0]
+        contained_node = \
+            cfy_local.storage.get_node_instances('contained_node')[0]
         print contained_node
 
-        self.assertEqual(direct.runtime_properties['controller_queue'], 'direct')
-        self.assertEqual(host_none.runtime_properties['controller,queue'], '')
-        self.assertEqual(connected_host.runtime_properties['controller_queue'], 'queue')
-        self.assertEqual(direct_override.runtime_properties['controller_queue'], 'direct_override')
-        self.assertEqual(contained_node.runtime_properties['controller_queue'], 'father_host')
+        self.assertEqual(
+            direct.runtime_properties['controller_queue'],
+            'direct')
+        self.assertEqual(
+            host_none.runtime_properties['controller,queue'],
+            '')
+        self.assertEqual(
+            connected_host.runtime_properties['controller_queue'],
+            'queue')
+        self.assertEqual(
+            direct_override.runtime_properties['controller_queue'],
+            'direct_override')
+        self.assertEqual(
+            contained_node.runtime_properties['controller_queue'],
+            'father_host')
