95a231bf679a06dedd89455b335106ef2fe09da7
Maxim Orlov <maxim@gigaspaces.com>
2017-09-27 18:51:32 +0300

    CFY-7304-Bump-the-wagon-version-requirement-to-0.6.1 (#365)

--- packaging/provision.sh
+++ packaging/provision.sh
@@ -43,7 +43,7 @@ function install_wagon(){
     else
         echo 'probably windows machine'
     fi
-    pip install wagon[venv]==0.6.0
+    pip install wagon[venv]==0.6.1
 }
 
 function wagon_create_package(){
