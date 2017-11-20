e03fba17c8f76ba92d98f09501fc3923fb892110
limor-gs <limor@gigaspaces.com>
2017-11-20 17:28:32 +0200

    Add GITHUB_ORGANIZATION and CONSTRAINTS_FILE variables to the build process, add constraints file (#367)

--- packaging/provision.sh
+++ packaging/provision.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
 SUDO=""
 
@@ -18,10 +18,10 @@ function install_dependencies(){
     echo "## Installing necessary dependencies"
 
     if  which yum; then
-        sudo yum -y install python-devel gcc openssl git libxslt-devel libxml2-devel openldap-devel libffi-devel openssl-devel
+        sudo yum -y install python-devel gcc openssl git libxslt-devel libxml2-devel openldap-devel libffi-devel openssl-devel libvirt-devel
     elif which apt-get; then
         sudo apt-get update &&
-        sudo apt-get -y install build-essential python-dev gcc openssl libffi-dev libssl-dev
+        sudo apt-get -y install build-essential python-dev gcc openssl libffi-dev libssl-dev libvirt-dev
     else
         echo 'probably windows machine'
         pip install virtualenv
@@ -43,15 +43,16 @@ function install_wagon(){
     else
         echo 'probably windows machine'
     fi
-    pip install wagon[venv]==0.3.2
+    pip install --upgrade pip==9.0.1
+    pip install wagon==0.3.2
 }
 
 function wagon_create_package(){
 
     echo "## wagon create package"
     if [[ "$PLUGIN_NAME" =~ "softlayer" ]]; then
-        echo "git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/cloudify-cosmo/$PLUGIN_NAME.git"
-        git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/cloudify-cosmo/$PLUGIN_NAME.git
+        echo "git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME.git"
+        git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME.git
         pushd $PLUGIN_NAME
             if [ "$PLUGIN_TAG_NAME" == "master" ];then
                 git checkout master
@@ -62,7 +63,11 @@ function wagon_create_package(){
         mkdir create_wagon ; cd create_wagon
         wagon create -s ../$PLUGIN_NAME/
     else
-        wagon create -s https://github.com/cloudify-cosmo/$PLUGIN_NAME/archive/$PLUGIN_TAG_NAME.tar.gz --validate -v -f
+        if [ ! -z "$CONSTRAINTS_FILE" ] && [ -f "/vagrant/$CONSTRAINTS_FILE" ];then
+            wagon create -s https://github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME/archive/$PLUGIN_TAG_NAME.tar.gz --validate -v -f -a '--no-cache-dir -c /vagrant/'$CONSTRAINTS_FILE''
+        else
+            wagon create -s https://github.com/$GITHUB_ORGANIZATION/$PLUGIN_NAME/archive/$PLUGIN_TAG_NAME.tar.gz --validate -v -f
+        fi     
     fi
 }
 
@@ -82,6 +87,8 @@ AWS_ACCESS_KEY=$4
 PLUGIN_NAME=$5
 PLUGIN_TAG_NAME=$6
 PLUGIN_S3_FOLDER=$7
+GITHUB_ORGANIZATION=$8
+CONSTRAINTS_FILE=$9
 
 export AWS_S3_BUCKET="cloudify-release-eu"
 export AWS_S3_PATH="cloudify/wagons/$PLUGIN_NAME/$PLUGIN_S3_FOLDER"
